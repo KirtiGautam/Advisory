362238a480cc288a632643d2a04f8c18faa9d51c
KirtiGautam <gautamkirti8c@gmail.com>
2020-03-09 14:21:34 +0530

    Refractoring

--- Advisory/settings.py
+++ Advisory/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1h)++rp5^exich=on=qq&wxp81*1esq79@x3ep)^j(%p$k9b*m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.43.184', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.43.10', '127.0.0.1']
 
 
 # Application definition
