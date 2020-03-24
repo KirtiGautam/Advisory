2a03bdd44c87fea49512ebcaa53b3a2b3d1e05da
RiyaSingh15 <r.riyasingh.s15@gmail.com>
2020-03-24 20:28:56 +0530

    Color schema change

--- Static/js/all.js
+++ Static/js/all.js
@@ -1,3 +1,16 @@
+//Navbar
+
+function openSlideMenu(){
+    document.getElementById('menu').style.width = '250px';
+    document.getElementById('content').style.marginLeft = '250px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('content').style.marginLeft = '0';
+}
+
+
 //Data Upload
 $(document).ready(function () {
 
