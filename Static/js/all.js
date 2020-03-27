a89572949dec74fa63bf20ffa04e81d0ea84cded
RiyaSingh15 <r.riyasingh.s15@gmail.com>
2020-03-27 20:24:15 +0530

    nav fix

--- Static/js/all.js
+++ Static/js/all.js
@@ -1,11 +1,18 @@
 //Navbar
 
-function openSlideMenu(){
-    document.getElementById('menu').style.width = '250px';
-    document.getElementById('content').style.marginLeft = '250px';
+function SlideMenu() {
+    if (document.getElementById('menu').style.width == '250px') {
+        document.getElementById('menu').style.width = '0';
+        document.getElementById('content').style.marginLeft = '0';
+    }
+
+    else {
+        document.getElementById('menu').style.width = '250px';
+        document.getElementById('content').style.marginLeft = '250px';
+    }
 }
 
-function closeSlideMenu(){
+function closeSlideMenu() {
     document.getElementById('menu').style.width = '0';
     document.getElementById('content').style.marginLeft = '0';
 }
